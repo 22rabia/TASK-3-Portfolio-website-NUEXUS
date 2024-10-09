76a6feadd91b13efa7296f38bb21caff14d3279d
Rabia Kabir <139744497+22rabia@users.noreply.github.com>
2024-10-09 20:07:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener('scroll', function () {
+    var topButton = document.getElementById('top');
+    if (window.scrollY > 300) {
+        topButton.style.display = 'block';
+    } else {
+        topButton.style.display = 'none';
+    }
+});
+
+// Smooth scrolling for all links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle between dark and light themes
+document.getElementById('icon').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.classList.toggle('dark-theme');
+    // Toggle the icon between sun and moon
+    const icon = document.querySelector('#icon i');
+    if (document.body.classList.contains('dark-theme')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
